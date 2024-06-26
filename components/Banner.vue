6cd3d5ebcda4381a93c4afeacbd28c5b7244fde6
webdev1881 <webdev1881@gmail.com>
2024-06-26 16:52:51 +0300

    qwqw

--- components/Banner.vue
+++ components/Banner.vue
@@ -7,7 +7,7 @@
         Test assignment for front-end developer
       </h1>
       <div class="banner-content--description">
-        What defines a good front-end developer is one that has skilled
+        999What defines a good front-end developer is one that has skilled
         knowledge of HTML, CSS, JS with a vast understanding of User design
         thinking as they'll be building web interfaces with accessibility in
         mind. They should also be excited to learn, as the world of Front-End
